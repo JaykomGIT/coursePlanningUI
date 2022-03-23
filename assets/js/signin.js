187e26a47ff8fe104cdb6fee8f700c9a6a9591b4
Erik Gill-Fisher <erikgill98@yahoo.com>
2022-03-23 16:05:50 -0400

    login UI
    added login UI with a little JavaScript

---
+++ assets/js/signin.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-button");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "test@temple.edu" && password === "test") {
+        location.reload();
+    } else {
+        alert("Incorrect Login. Please try again.");
+        loginErrorMsg.style.opacity = 1; 
+    }
+})
